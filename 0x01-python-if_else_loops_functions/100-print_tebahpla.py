6476efa81e038cb550c331193eeab335679439f6
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-08-23 14:16:49 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end=''
+for i in range(0, 26, 2):
+    print("{:c}{:c}".format(122 - i, (122 - i - 1) - 32), end='')

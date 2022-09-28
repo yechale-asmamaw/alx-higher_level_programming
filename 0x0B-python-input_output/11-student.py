9c3d8fe0fcc42c4ba08356eb2f316ab0ed320e34
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-09-28 13:48:09 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
 """
-Contains the clas "Student"
+Class Module
 """
 
 
 class Student:
-    """Representation of a student"""
+    """Student class
+    """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """initialize method"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
-
-    def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+    def to_json(self):
+        """retrieves a dictionary representation of student instance
+        """
+        return vars(self)
